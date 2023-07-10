17649977eeabd0607f0554e0d821aeb4495c91e3
Ankita Thakur <ankitathakurankita7007@gmail.com>
2023-07-10 15:13:30 +0530

    add user model,routes

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const { connection } = require("./config/db");
+const { user } = require("./routes/user.routes");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+//user route
+app.use("/user", user);
+
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("Connected to db");
+  } catch (err) {
+    console.log(err);
+    console.log("Error connecting in database");
+  }
+
+  console.log(`Server is running at port ${process.env.port}`);
+});
